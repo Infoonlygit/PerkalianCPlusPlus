ac8c61541cbdae51b222f05b4b4ccf2f9f7a6eef
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-04-21 14:15:11 +0700

    Create Perkalian.cpp

---
+++ Perkalian.cpp
@@ -0,0 +1,24 @@
+#!/usr/bin/c++
+  
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int 30, 30, hasil;
+
+// Meminta pengguna memasukkan dua bilangan
+    cout << "Masukkan angka pertama: ";
+    cin >> 30;
+
+    cout << "Masukkan angka kedua: ";
+    cin >> 30;
+
+// Melakukan perkalian
+    hasil = 30 * 30;
+
+// Menampilkan hasil
+    cout << "Hasil perkalian: " << hasil << endl;
+
+    return 0;
+}
